bc93b4af39e39ecf902b47859e0f15ee14c41914
Team 4237 Laptop 3 <LakeshoreRoboticsTeam@gmail.com>
2025-02-19 17:49:53 -0500

    NP GH - spacing in Driver Tab

--- src/main/java/frc/robot/util/DriverTab.java
+++ src/main/java/frc/robot/util/DriverTab.java
@@ -17,16 +17,14 @@ import frc.robot.subsystems.Drivetrain;
 // import frc.robot.subsystems.Intake;
 import frc.robot.RobotContainer;
 
-
-public class DriverTab
-{
+public class DriverTab {
     // This string gets the full name of the class, including the package name
     private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
-   // private static PoseEstimator poseEstimator = new PoseEstimator<>(null, null, null, null);
+    // private static PoseEstimator poseEstimator = new PoseEstimator<>(null, null,
+    // null, null);
     // *** STATIC INITIALIZATION BLOCK ***
     // This block of code is run first when the class is loaded
-    static
-    {
+    static {
         System.out.println("Loading: " + fullClassName);
     }
 
@@ -35,28 +33,23 @@ public class DriverTab
     // private CommandSwerveDrivetrain drivetrain;
     private Pivot pivot;
     // private Intake intake;
-    //private IntakePositioning intakePositioning;
+    // private IntakePositioning intakePositioning;
     private PoseEstimator poseEstimator;
 
     private String intakeString = " ";
-    
+
     private GenericEntry pivotAngleBox;
     // private GenericEntry intakeStatusBox;
     private ComplexWidget fieldBox;
     private Drivetrain drivetrain;
-   
-    
- 
 
     // *** CLASS CONSTRUCTOR ***
-    DriverTab(RobotContainer robotContainer)
-    {
+    DriverTab(RobotContainer robotContainer) {
         System.out.println("  Constructor Started:  " + fullClassName);
 
-        
         // createPivotAngleBox();
         // createIntakeStatusBox();
-    
+
         // this.pivot = robotContainer.pivot;
         // this.intake = robotContainer.intake;
         // this.intakePositioning = robotContainer.intakePositioning;
@@ -66,88 +59,79 @@ public class DriverTab
         // return driverTab.add("Field", field)
         // .withWidget(BuiltInWidgets.kField) //specifies type of widget: "kField
         // .withPosition(7,1) // sets position of widget
-        // .withSize(19,12);  // sets size of widget
-        
+        // .withSize(19,12); // sets size of widget
 
         // if(pivot != null)
         // {
-        //     pivotAngleBox = createPivotAngleBox();
+        // pivotAngleBox = createPivotAngleBox();
         // }
 
         // if(intakePositioning != null)
         // {
-        //     intakeStatusBox = createIntakeStatusBox();
+        // intakeStatusBox = createIntakeStatusBox();
         // }
 
         // if(drivetrain != null)
         // {
-        //     createFieldBox();
+        // createFieldBox();
         // }
 
         System.out.println("  Constructor Finished: " + fullClassName);
     }
- 
+
     // private ComplexWidget createFieldBox()
     // {
-    //     field.setRobotPose(drivetrain.getPose());
-    //     return driverTab.add("Field", field)
-    //     .withWidget(BuiltInWidgets.kField) //specifies type of widget: "kField
-    //     .withPosition(7,1) // sets position of widget
-    //     .withSize(19,12);  // sets size of widget
+    // field.setRobotPose(drivetrain.getPose());
+    // return driverTab.add("Field", field)
+    // .withWidget(BuiltInWidgets.kField) //specifies type of widget: "kField
+    // .withPosition(7,1) // sets position of widget
+    // .withSize(19,12); // sets size of widget
     // }
-
-    
 
     // private GenericEntry createPivotAngleBox()
     // {
-    //     return driverTab.add("Pivot Angle", round(pivot.getMotorEncoderPosition(),3))
-    //     .withWidget(BuiltInWidgets.kTextView) //specifies type of widget: "kTextView"
-    //     .withPosition(1,2) // sets position of widget
-    //     .withSize(4,2)  // sets size of widget
-    //     .getEntry();
+    // return driverTab.add("Pivot Angle", round(pivot.getMotorEncoderPosition(),3))
+    // .withWidget(BuiltInWidgets.kTextView) //specifies type of widget: "kTextView"
+    // .withPosition(1,2) // sets position of widget
+    // .withSize(4,2) // sets size of widget
+    // .getEntry();
     // }
-
 
     // private GenericEntry createIntakeStatusBox()
     // {
 
+    // return driverTab.add("Intake Status", "Down")
+    // .withWidget(BuiltInWidgets.kTextView)
+    // .withPosition(1, 8)
+    // .withSize(5, 2)
+    // .getEntry();
 
-    //     return driverTab.add("Intake Status", "Down")
-    //     .withWidget(BuiltInWidgets.kTextView)
-    //     .withPosition(1, 8)
-    //     .withSize(5, 2)
-    //     .getEntry();
-       
-            
     // }
 
-    
-    public void updateData()
-    {
+    public void updateData() {
 
         // if(pivot != null)
         // {
-        //     pivotAngleBox.setDouble(round(pivot.getCANCoderAngle(), 3));
+        // pivotAngleBox.setDouble(round(pivot.getCANCoderAngle(), 3));
         // }
 
         // if(intake != null)
         // {
-        //     if(intakePositioning.isIntakeUp())
-        //     {
-        //         intakeString = "Up";
-        //     }
-        //     else if (intakePositioning.isIntakeDown())
-        //     {
-        //         intakeString = "Down";
-        //     }
-            
-        //     intakeStatusBox.setString(intakeString);
+        // if(intakePositioning.isIntakeUp())
+        // {
+        // intakeString = "Up";
         // }
-     
+        // else if (intakePositioning.isIntakeDown())
+        // {
+        // intakeString = "Down";
+        // }
+
+        // intakeStatusBox.setString(intakeString);
+        // }
+
     }
 
-    public double round(double value, int digits)
-    {
+    public double round(double value, int digits) {
         double x = Math.pow(10.0, digits);
         return Math.round(value * x) / x;
     }
