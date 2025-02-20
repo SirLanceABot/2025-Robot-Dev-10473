a8621a7def0b2fb83b08d3a4ad7f777c10dd2bb9
Jon Woodard <jwoodard@lpslancer.net>
2025-02-20 10:13:07 -0500

    created ElasticLance to send SmartDashboard data

---
+++ src/main/java/frc/robot/elastic/ElasticLance.java
@@ -0,0 +1,38 @@
+package frc.robot.elastic;
+
+import java.lang.invoke.MethodHandles;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class ElasticLance 
+{
+    // This string gets the full name of the class, including the package name
+    private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
+
+    // *** STATIC INITIALIZATION BLOCK ***
+    // This block of code is run first when the class is loaded
+    static
+    {
+        System.out.println("Loading: " + fullClassName);
+    }
+
+    // *** CLASS VARIABLES & INSTANCE VARIABLES ***
+    // Put all class variables and instance variables here
+
+    private ElasticLance()
+    {}
+
+    public static void sendDataToSmartDashboard()
+    {
+        SmartDashboard.putNumber("Voltage", RobotController.getBatteryVoltage());
+        SmartDashboard.putNumber("Match Time", DriverStation.getMatchTime());
+        SmartDashboard.putNumber("CAN Utilization %", RobotController.getCANStatus().percentBusUtilization * 100.00);
+        SmartDashboard.putNumber("CPU Temperature", RobotController.getCPUTemp());
+        // SmartDashboard.putNumber("Pivot", Pivot.getPosition());
+
+        // SmartDashboard.putNumber(":)", LEDs.getLEDs());
+        //SmartDashboard.putBoolean("Alerts Working", Alerts)
+    }
+}
