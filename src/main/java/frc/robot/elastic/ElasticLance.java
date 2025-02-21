b089df6750bcf13376af5da101f4a322cb47d3fe
Team 4237 Laptop 3 <LakeshoreRoboticsTeam@gmail.com>
2025-02-21 17:04:06 -0500

    NP GH PB - updated elastic Auto tab

--- src/main/java/frc/robot/elastic/ElasticLance.java
+++ src/main/java/frc/robot/elastic/ElasticLance.java
@@ -2,15 +2,19 @@ package frc.robot.elastic;
 
 import java.lang.invoke.MethodHandles;
 
+import javax.lang.model.util.ElementScanner14;
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color;
 
 public class ElasticLance 
 {
     // This string gets the full name of the class, including the package name
     private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
 
+    private static Color color = new Color();
     // *** STATIC INITIALIZATION BLOCK ***
     // This block of code is run first when the class is loaded
     static
@@ -24,15 +28,43 @@ public class ElasticLance
     private ElasticLance()
     {}
 
+    // public static void createWidgets()
+    // {
+    //     updateAllianceColorBox();
+    // }
+
     public static void sendDataToSmartDashboard()
     {
         SmartDashboard.putNumber("Voltage", RobotController.getBatteryVoltage());
         SmartDashboard.putNumber("Match Time", DriverStation.getMatchTime());
         SmartDashboard.putNumber("CAN Utilization %", RobotController.getCANStatus().percentBusUtilization * 100.00);
         SmartDashboard.putNumber("CPU Temperature", RobotController.getCPUTemp());
+
+        updateAllianceColorBox();
+        // SmartDashboard.putString("Alliance Color", color.toHexString());
         // SmartDashboard.putNumber("Pivot", Pivot.getPosition());
 
         // SmartDashboard.putNumber(":)", LEDs.getLEDs());
         //SmartDashboard.putBoolean("Alerts Working", Alerts)
+
+        // SmartDashboard.put("Alliance Color", DriverStation.getAlliance());
+    }
+
+    public static void updateAllianceColorBox()
+    {
+        if(DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == DriverStation.Alliance.Red)
+        {
+            color = Color.kRed;
+        }
+        else if(DriverStation.getAlliance().isPresent() && DriverStation.getAlliance().get() == DriverStation.Alliance.Blue)
+        {
+            color = Color.kBlue;
+        }
+        else 
+        {
+            color = Color.kGray;
+        }
+
+        SmartDashboard.putString("Alliance Color", color.toHexString());
     }
 }
