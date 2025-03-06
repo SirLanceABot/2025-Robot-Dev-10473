c094e96216248317ccbe61c914df7e375fb58bed
Team 4237 Laptop 4 <LakeshoreRoboticsTeam@gmail.com>
2025-03-05 22:37:24 -0500

    RJ ☠️

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -30,7 +30,7 @@ public class RobotContainer
         System.out.println("Loading: " + fullClassName);
     }
 
-    private boolean useFullRobot            = true;
+    private boolean useFullRobot            = false;
     private boolean usePivot                = false;
     private boolean useDrivetrain           = false;
     private boolean useRoller               = false;
