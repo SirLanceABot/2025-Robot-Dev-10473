ab8dd94617a5ac96ddfd17b4cd0ae9a632654b03
Team 4237 Laptop 4 <LakeshoreRoboticsTeam@gmail.com>
2025-03-07 13:44:22 -0500

    merge stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -30,7 +30,7 @@ public class RobotContainer
         System.out.println("Loading: " + fullClassName);
     }
 
-    private boolean useFullRobot            = false;
+    private boolean useFullRobot            = true;
     private boolean usePivot                = false;
     private boolean useDrivetrain           = false;
     private boolean useRoller               = false;
